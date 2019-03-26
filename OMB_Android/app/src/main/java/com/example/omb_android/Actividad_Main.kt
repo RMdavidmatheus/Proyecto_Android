124ea06e517d27b92eb725dad91c89e9cf0819dd
David Mateus <damateus964@misena.edu.co>
2019-03-26 16:21:23 -0500

    Desde inicio, ninguna logica implementada, faltan diseños

---
+++ OMB_Android/app/src/main/java/com/example/omb_android/Actividad_Main.kt
@@ -0,0 +1,12 @@
+package com.example.omb_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Actividad_Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ OMB_Android/app/src/main/java/com/example/omb_android/Login_Activity.kt
@@ -0,0 +1,18 @@
+package com.example.omb_android
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Login_Activity : AppCompatActivity() {
+
+    fun Formulario_Registrar(view: View){
+        startActivity(Intent(this, Register_Activity::class.java))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_)
+    }
+}
